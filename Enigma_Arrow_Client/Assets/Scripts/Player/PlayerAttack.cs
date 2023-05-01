2f61b54a6a9befff5b5e1e3a21c505ca5e614af8
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-01 19:50:39 +0900

    Fire Logic

--- Enigma_Arrow_Client/Assets/Scripts/Player/PlayerAttack.cs
+++ Enigma_Arrow_Client/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,39 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField] GameObject bulletObj;
-    [SerializeField] private float FireDelayTime;
-    float fireTimer;
-    
-    // Start is called before the first frame update
+    [SerializeField] float _speed;
+    private float _startVecY;       // 공격 시작 위치
+
+    [Header("Fire")]
+    [SerializeField] GameObject _bulletObj;
+    [SerializeField] private float _FireDelayTime;
+    [SerializeField] float _fireTimer = 0;
+
     void Start()
     {
-        
+        _startVecY = transform.eulerAngles.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        AttackMove();
 
-    public void AttackBtnClick()
-    {
-        if(fireTimer >= 0)      // 공격 쿨타임 중일때 
+        if(Input.GetMouseButtonDown(0))     // test
         {
-
+            AttackBtnClick();
         }
     }
 
     /// <summary>
-    /// 총알 발사 
+    /// 공격 방향 움직임 
+    /// </summary>
+    private void AttackMove()
+    {
+        Vector3 rotVec =
+        new Vector3(transform.rotation.x,
+        Mathf.PingPong(Time.time * _speed, 180) + _startVecY,
+        transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotVec);
+    }
+
+    #region 공격
+
+
+    /// <summary>
+    /// 공격 입력
+    /// </summary>
+    public void AttackBtnClick()
+    {
+        if (_fireTimer > 0)      // 공격 쿨타임 중일때 
+        {
+            Debug.Log("아직 쿨타임 중입니다");
+            return;
+        }
+
+        StartCoroutine(AttckCoroutine());
+    }
+
+    /// <summary>
+    /// 공격 체크 코루틴
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AttckCoroutine()
+    {
+        Fire();
+        while (true)
+        {
+            _fireTimer += Time.deltaTime;
+            if(_fireTimer >= _FireDelayTime)
+            {
+                _fireTimer = 0;
+                yield break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// 총알 발사 (실제 공격)
     /// </summary>
     private void Fire()
     {
-
+        _fireTimer = 0;
+        Debug.Log("ㄹㄴㅇㄹ");
     }
+
+    #endregion
 }
