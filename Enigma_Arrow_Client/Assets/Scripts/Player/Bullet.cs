1ca157a3c85164c69f24260ce73a4dda0f49e7bf
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-02 13:44:17 +0900

    Update Bullet.cs
    Bullet ObjPool

--- Enigma_Arrow_Client/Assets/Scripts/Player/Bullet.cs
+++ Enigma_Arrow_Client/Assets/Scripts/Player/Bullet.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Pool;  
 
 public class Bullet : MonoBehaviour
 {
     Rigidbody rigid;
     [SerializeField] float _speed = 70;      // 총알 스피드
+
+    [Header("Pool")]
+    private IObjectPool<Bullet> _managedPool;
 
     private void Awake()
     {
@@ -14,7 +19,28 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(DestroyCoroutine());
        rigid.AddForce(transform.forward * _speed,ForceMode.Impulse);
         
+    }
+
+    IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        DestroyBullet();
+    }
+
+    /// <summary>
+    /// Pool Set
+    /// </summary>
+    /// <param name="pool"></param>
+    public void SetManagedPool(IObjectPool<Bullet> pool)
+    {
+        _managedPool= pool;
+    }
+
+    public void DestroyBullet()
+    {
+        _managedPool.Release(this);     //인스턴스를 다시 pool로 반환
     }
 }
