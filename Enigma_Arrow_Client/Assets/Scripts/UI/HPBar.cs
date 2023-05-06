0aebc7cd8256ceebf14908d2a603898ec5f1dc04
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-06 16:17:20 +0900

    HP UI  Slider Color

--- Enigma_Arrow_Client/Assets/Scripts/UI/HPBar.cs
+++ Enigma_Arrow_Client/Assets/Scripts/UI/HPBar.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class HPBar : MonoBehaviour
 {
     public Slider _slider;
+    public Gradient _gradient;
+    public Image _fill;
 
     public void SetMaxHP(int hp)
     {
         _slider.maxValue= hp;
         _slider.value= hp;
+
+        _fill.color = _gradient.Evaluate(1f);
     }
 
     public void SetHP(int hp)
     {
         _slider.value = hp;
+        _fill.color = _gradient.Evaluate(_slider.normalizedValue);
     }
 }
