0af80f509de053d50d159938b03d1c27838674ae
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-08 18:21:17 +0900

    Update OutComeUI.cs
    활성화 / 비활성화 관리

--- Enigma_Arrow_Client/Assets/Scripts/UI/OutComeUI.cs
+++ Enigma_Arrow_Client/Assets/Scripts/UI/OutComeUI.cs
@@ -7,8 +7,17 @@ public class OutComeUI : Singleton<OutComeUI>
 {
     [SerializeField] GameObject[] outcomePanels;
 
+    private void Start()
+    {
+        for (int i = 0; i < outcomePanels.Length; i++)
+        {
+            outcomePanels[i].SetActive(false);
+        }
+    }
+
     public void ShowOutcomePanel(bool win)
     {
         GameObject panel = win == true ? outcomePanels[0] : outcomePanels[1];
+        panel.SetActive(true);
     }
 }
