f761d4c379ba901ab0ff80cfbd7b23fe3aa1c6c6
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-08 17:48:48 +0900

    Update OutComeUI.cs
    싱글톤화

--- Enigma_Arrow_Client/Assets/Scripts/UI/OutComeUI.cs
+++ Enigma_Arrow_Client/Assets/Scripts/UI/OutComeUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
 
-public class OutComeUI : MonoBehaviour
+public class OutComeUI : Singleton<OutComeUI> 
 {
     [SerializeField] GameObject[] outcomePanels;
 
