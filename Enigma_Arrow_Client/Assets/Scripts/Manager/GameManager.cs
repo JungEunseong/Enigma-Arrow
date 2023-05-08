46b09e6989cc120fbadfeabc66ba95a9cb643b93
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-08 15:58:59 +0900

    GameManager

---
+++ Enigma_Arrow_Client/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 게임이 종료된 상황
+    /// </summary>
+    /// <param name="win"></param>
+    public void GameOver(bool win)
+    {
+
+    }
+}
